050d71b52f7628e34f80f8b1ddc6ce485a2e3e50
nbdybtchu <163382681+nbdybtchu@users.noreply.github.com>
2024-06-29 08:31:13 +0800

    Puting typewriter effect

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -8,6 +8,8 @@ import linkedinLight from '../../assets/linkedin-light.svg';
 import linkedinDark from '../../assets/linkedin-dark.svg';
 import CV from '../../assets/Booker Edward Arib.pdf';
 import { useTheme } from '../../common/ThemeContext';
+import { motion } from 'framer-motion';
+import { Typewriter } from 'react-simple-typewriter';
 
 function Hero() {
   const { theme, toggleTheme } = useTheme();
@@ -32,20 +34,33 @@ function Hero() {
         />
       </div>
       <div className={styles.info}>
+      <motion.div
+      whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+      transition={{ duration: 0.5 }}
+      >
         <h1>
-          Chu
-          <br />
-          Arib
+          Chu Arib
         </h1>
-        <h2>Frontend Developer</h2>
-        <span>
-          <a href="https://github.com/nbdybtchu" target="_blank">
-            <img src={githubIcon} alt="Github icon" />
-          </a>
-          <a href="https://www.linkedin.com/in/chuarib/" target="_blank">
-            <img src={linkedinIcon} alt="Linkedin icon" />
-          </a>
-        </span>
+        <h2>
+            <Typewriter 
+            loop
+            cursor
+            cursorStyle='_'
+            typeSpeed={60}
+            deleteSpeed={50}
+            delaySpeed={1500}
+            words={['Frontend Developer', 'UX/UI Designer', 'Tattoo Artist', 'Dad']}
+            />
+        </h2>
+        </motion.div>
+            <span>
+                <a href="github.com/nbdybtchu" target="_blank">
+                    <img src={githubIcon} alt="Github icon" />
+                </a>
+                <a href="www.linkedin.com/in/chuarib/" target="_blank">
+                    <img src={linkedinIcon} alt="Linkedin icon" />
+                </a>
+            </span>
         <p className={styles.description}>
           With a passion for developing modern React web apps for commercial
           businesses.
